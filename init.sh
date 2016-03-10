b975eb3277185a6892765a714a24311042072307
Homma Takuma <sfprhythnn@gmail.com>
2016-03-11 02:54:35 +0900

    corresponds to Ansible 2

--- init.sh
+++ init.sh
@@ -2,8 +2,7 @@
 sudo xcodebuild -license
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew doctor
-brew install ansible git
-git clone https://github.com/rhythnn/mac-ansible
+brew install ansible
 echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile
 source ~/.bash_profile
 ansible-playbook -i hosts -vv main.yml
