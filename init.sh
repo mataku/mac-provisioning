decb0c39cf3122e3a6a4517bd4617e0157471d83
Homma Takuma <sfprhythnn@gmail.com>
2016-05-19 20:37:48 +0900

    AAHhhhh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+osascript -e 'display notification "enter your password!" with title "Xcode license requires"'
 sudo xcodebuild -license
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew doctor
-brew install ansible
+brew install git ansible
 echo 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"' >> ~/.bash_profile
 source ~/.bash_profile
 ansible-playbook -i hosts -vv main.yml
